38955caa1e1a72495a38239ab5e5304e177889eb
Alex Pecanha <alexpec@gmail.com>
2017-05-02 22:40:23 -0300

    Working on issue #6      + Creating test_CaseTagReplacer;

---
+++ OFCase/test/test_CaseTagReplacer.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+
+
+
+@pytest.fixture(scope='module')
+def create_model_dir(tmpdir_factory):
+    pass
+
+
+def test_CaseTagReplacer(create_model_dir):
+    #Creating the Parameters
+    pass
+    
