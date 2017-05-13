ade96937b4b2f3d50d0478496c7d4cf35d690e1b
Alex Pecanha <alexpec@gmail.com>
2017-05-13 07:54:08 -0300

    Working on issue #15      * closes #17;      + Incorporate in SplitMeshRegions the parser context;      + Incorporate in SplitMeshRegions the possible Errors;      + Adjust tests to new model;

--- OFCase/SplitMeshRegions.py
+++ OFCase/SplitMeshRegions.py
@@ -1,5 +1,6 @@
 from Utils.TerminalRunner import TerminalRunner
 import os
+from Utils.REParser import REParser
 
 
 
@@ -17,12 +18,37 @@ class SplitMeshRegions(TerminalRunner):
     def buffer(self):
         return super(SplitMeshRegions, self).buffer
     
+    @property
+    def parser(self):
+        return self._parser
+    
+    @property
+    def parse_message(self):
+        return self._parseMessage
+    
+    
+    def _parseMessage(self, match, lines, index):
+        return match.group(0)
+    
+    
+    def _setUpParser(self):
+        self._parser.AddPattern('E#1', "Exposed face: \d* fc: \d* has owner region (.*) and neighbour region (.*) when handling region: (.*)", self._parseMessage)
+        self._parser.AddPattern('E#2', "Cell \d* with cell centre \d* is multiple zones. This is not allowed.", self._parseMessage)
+        self._parser.AddPattern('E#3', "cellZones not synchronised across processors.", self._parseMessage)
+        self._parser.AddPattern('E#4', "You cannot specify both -cellZonesOnly or -cellZonesFileOnly", self._parseMessage)
+        self._parser.AddPattern('E#5', "You cannot specify both -largestOnly", self._parseMessage)
+        self._parser.AddPattern('E#6', "For the cellZonesOnly option all cells have to be in a cellZone;", self._parseMessage)
+        self._parser.AddPattern('E#7', "For the cellZonesFileOnly options all cells have to be in a cellZone.", self._parseMessage)
+        self._parser.AddPattern('E#8', "Point \d* is not inside the mesh.", self._parseMessage)
+        
     
     def __init__(self, ofcase_path, of_environment_cmd=None):
         super(SplitMeshRegions, self).__init__()
         
         self._of_environment_cmd = of_environment_cmd
         self._ofcase_path = ofcase_path
+        
+        self._parser = REParser()
         
     def Runner(self, cellzones=True, overwrite=False):
         main_cmd = 'splitMeshRegion'
@@ -41,3 +67,8 @@ class SplitMeshRegions(TerminalRunner):
             cmd = path_cmd
             
         super(SplitMeshRegions, self).Runner(cmd)
+        
+    
+    def ParseBuffer(self):
+        results = self.parser.ParseBuffer(self.buffer)
+        return results
--- OFCase/test/test_SplitMeshRegions.py
+++ OFCase/test/test_SplitMeshRegions.py
@@ -29,30 +29,14 @@ def parse_message(match, lines, index):
     return match.group(0)
 
 
-@pytest.fixture
-def Parser():
-    parser = REParser()
-    
-    parser.AddPattern('E#1', "Exposed face: \d* fc: \d* has owner region (.*) and neighbour region (.*) when handling region: (.*)", parse_message)
-    parser.AddPattern('E#2', "Cell \d* with cell centre \d* is multiple zones. This is not allowed.", parse_message)
-    parser.AddPattern('E#3', "cellZones not synchronised across processors.", parse_message)
-    parser.AddPattern('E#4', "You cannot specify both -cellZonesOnly or -cellZonesFileOnly", parse_message)
-    parser.AddPattern('E#5', "You cannot specify both -largestOnly", parse_message)
-    parser.AddPattern('E#6', "For the cellZonesOnly option all cells have to be in a cellZone;", parse_message)
-    parser.AddPattern('E#7', "For the cellZonesFileOnly options all cells have to be in a cellZone.", parse_message)
-    parser.AddPattern('E#8', "Point \d* is not inside the mesh.", parse_message)
-    
-    return parser
 
-
-def test_SplitMeshRegions(monkeypatch, Parser):
+def test_SplitMeshRegions(monkeypatch):
     curr_dir = os.path.dirname(os.path.abspath(__file__))
     module_dir, _ = os.path.split(curr_dir)
     package_dir = os.path.split(module_dir)[0]
     
     case_dir = os.path.join(package_dir, '_data/Tagged')
     
-    par = Parser
     smr = SplitMeshRegions(case_dir)
     smr_output_file = os.path.join(package_dir, '_data/Logs_OFRun/log.splitMeshRegions')
     
@@ -76,8 +60,7 @@ def test_SplitMeshRegions(monkeypatch, Parser):
     monkeypatch.setattr("subprocess.Popen", monkey)
     
     smr.Runner()
-    results = par.ParseBuffer(smr.buffer)
-    
+    results = smr.ParseBuffer()
     assert True
     
     
